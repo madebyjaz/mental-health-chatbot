561ebc1f7dacef28734bd6884b57d9ebf142680b
Jazsmin Gilliam <jazzgeeter.dev@gmail.com>
2025-11-19 15:48:47 -0600

    Implemented the compute_metrics & the training functions
    ~ Fixed the emotion classifier model to google/electra-base-discriminator
    ~ Completed the compute_metrics function w/accuracy, precision, recall, F1, & AUROC
    ~ Added the AUROC calculation w/error handling for multiclass tasks
    ~ Implemented train function w/model & tokenizer initialization
    ~ Added the loading dataset for the go_emotions & the dreaddit datasets
    ~ Implemented a preprocessing function w/tokenization
    ~ Configured the encoded dataset w/torch format for the training pipeline

--- src/classifiers/training.py
+++ src/classifiers/training.py
@@ -12,7 +12,7 @@ from sklearn.metrics import accuracy_score, precision_recall_fscore_support, roc
 # three inntende classification tasks: emotion, intent, risk
 TASKS = {
     "emotion": {
-        "model_name": "google/electra-base-goemotions",     # previously SamLowe/roberta-base-go_emotions could be google/electra-base-goemotions
+        "model_name": "google/electra-base-discriminator",     # wrote the wrong model name for this classifier in the previous version
         "num_labels": 28,                               # 27 emotion labels + 1 neutral
         "problem_type": "multilabel_classification",
         "dataset": "go_emotions",
@@ -34,6 +34,59 @@ TASKS = {
 }
 
 def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    preds = predictions.argmax(-1)
 
-    if TASKS["dataset"] == "go_emotions":
+    ac = accuracy_score(labels, preds)
+    precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average = 'weighted')
+    metrics = {
+        "accuracy": ac,
+        "precision": precision, 
+        "recall": recall, 
+        "f1": f1,
+    }
+
+    try: 
+        auroc = roc_auc_score(labels, predictions[:,1])
+        metrics["auroc"] = auroc
+
+    except ValueError:  
+        raise ValueError("AUROC is not able to be computed for multiclass classification tasks.")
+        pass
+
+    return metrics
+
+def train(task, train_file = None, val_file = None,  output_dir = "./models/"):
+
+    cfg = TASKS[task]
+    tokenizer = AutoTokenizer.from_pretrained(cfg["model_name"])
+    model = AutoModelForSequenceClassification.from_pretrained(
+        cfg["model_name"],
+        num_labels = cfg["num_labels"],
+    )
+
+    if cfg["dataset"] == "go_emotions":
         dataset = lds("go_emotions")
+    elif cfg["dataset"] == "asmaab/dreaddit":
+        dataset = lds("asmaab/dreaddit")
+    else:
+        raise ValueError("‼️ The dataset your're trying to train on is not supported.")
+    
+    def preorocess(batch):
+        tokenize = tokenizer(batch["text"], padding="max_length", truncation=True, max_length=256)
+        if "labels" in batch:
+            tokenize["labels"] = batch["labels"]
+        return tokenize
+    
+    encoded_ds = dataset.map(preorocess, batched=True)
+
+    cols = ["input_ids", "attention_mask"]
+    if "labels" in encoded_ds["train"].column_names:
+        cols.append("labels")
+
+    encoded_ds.set_format(
+        type="torch",
+        columns=cols
+    )
+
+
